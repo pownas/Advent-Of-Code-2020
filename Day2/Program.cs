ad19920be947e64eaebb9e5a10111b52261b5681
pownas <pownas@outlook.com>
2020-12-02 11:11:36 +0100

    Day2-Part1 - Rätt!
    Nu fick jag rätt på uppgiften.
    Ska dock städa koden lite mer. :+1:

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -9,50 +9,61 @@ int numberOfValidPass = 0;
 string[] splittedLine;
 string[] subSplitt;
 
-string searchStart;
-string searchEnd;
+string minChar;
+string maxChar;
 string searchCharacter;
-string searchPassword;
+string strPassword;
 
 foreach (var line in dataList)
 {
     splittedLine = line.Split(" ");
     subSplitt = splittedLine[0].Split("-");
 
-    searchStart = subSplitt[0];
-    searchEnd = subSplitt[1];
+    minChar = subSplitt[0];
+    maxChar = subSplitt[1];
     searchCharacter = splittedLine[1].Replace(":", "");
-    searchPassword = splittedLine[2];
-    //searchStart = "10"; //Test-pass
-    //searchEnd = "12";//Test-pass
-    //searchCharacter = "d"; //Test-pass
-    //searchPassword = "ddvddnmdnlvdddqdcqph"; //Test-pass
+    strPassword = splittedLine[2];
 
-    Console.WriteLine("Start: " + searchStart + ", End: " + searchEnd + ", Char: " + searchCharacter + ", Pass: " + searchPassword);
-    
-    if (validatePasswordPolicy(int.Parse(searchStart), int.Parse(searchEnd), char.Parse(searchCharacter), searchPassword))
+
+    if (validatePasswordPolicy(int.Parse(minChar), int.Parse(maxChar), char.Parse(searchCharacter), strPassword))
     {
-        Console.WriteLine("true");
+        Console.Write("true - ");
         numberOfValidPass++;
     }
     else
     {
-        Console.WriteLine("false");
+        Console.Write("false - ");
     }
+
+    Console.WriteLine("Min: " + minChar + ", Max: " + maxChar + ", Char: " + searchCharacter + ", Password: " + strPassword);
+
 }
+
 Console.WriteLine("Number of valid passwords: " + numberOfValidPass);
 
-bool validatePasswordPolicy(int start, int end, char character, string password)
+
+bool validatePasswordPolicy(int min, int max, char character, string password)
 {
     char[] charPassword = password.ToCharArray();
-    bool returnValue = false;
+    bool returnValue;
+    int numberOfChar = 0;
 
     for (int i = 0; i < charPassword.Length; i++)
     {
-        if (charPassword[i] == character && i >= start-1 && i < end)
+        if (charPassword[i] == character)
         {
-            returnValue = true;
+            numberOfChar++;
         }
     }
+
+    if (numberOfChar >= min && numberOfChar <= max)
+    {
+        returnValue = true;
+    }
+    else
+    {
+        returnValue = false;
+    }
+
     return returnValue;
 }
