17c5db0a569d426947711f6f85f6a11b12d019b9
pownas <pownas@outlook.com>
2020-12-02 11:59:31 +0100

    Added a copy of Day2-Part1

---
+++ Day02/Day2-Part2/Program.cs
@@ -0,0 +1,70 @@
+﻿using AdventOfCode2020;
+using System;
+using System.Collections.Generic;
+
+AppDataInput inputFile = new AppDataInput();
+List<string> dataList = inputFile.ReadData("Day02-input.txt"); //Read data from: "../../AdventOfCode2020/Data/{dataInputFile}"
+
+int numberOfValidPass = 0;
+
+string[] splittedLine;
+string[] subSplitt;
+
+string minChar;
+string maxChar;
+string searchCharacter;
+string strPassword;
+
+foreach (var line in dataList)
+{
+    splittedLine = line.Split(" ");
+    subSplitt = splittedLine[0].Split("-");
+
+    minChar = subSplitt[0];
+    maxChar = subSplitt[1];
+    searchCharacter = splittedLine[1].Replace(":", "");
+    strPassword = splittedLine[2];
+
+
+    if (validatePasswordPolicy(int.Parse(minChar), int.Parse(maxChar), char.Parse(searchCharacter), strPassword))
+    {
+        Console.Write("true - ");
+        numberOfValidPass++;
+    }
+    else
+    {
+        Console.Write("false - ");
+    }
+
+    Console.WriteLine("Min: " + minChar + ", Max: " + maxChar + ", Char: " + searchCharacter + ", Password: " + strPassword);
+
+}
+
+Console.WriteLine("Number of valid passwords: " + numberOfValidPass);
+
+
+bool validatePasswordPolicy(int min, int max, char character, string password)
+{
+    char[] charPassword = password.ToCharArray();
+    bool returnValue;
+    int numberOfChar = 0;
+
+    for (int i = 0; i < charPassword.Length; i++)
+    {
+        if (charPassword[i] == character)
+        {
+            numberOfChar++;
+        }
+    }
+
+    if (numberOfChar >= min && numberOfChar <= max)
+    {
+        returnValue = true;
+    }
+    else
+    {
+        returnValue = false;
+    }
+
+    return returnValue;
+}
