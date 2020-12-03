002abb95cccf88c2351bc87fa99fde26ac283d21
pownas <pownas@outlook.com>
2020-12-03 09:18:08 +0100

    Updated Part2 with the answer

--- Day03/Day03-Part2/Program.cs
+++ Day03/Day03-Part2/Program.cs
@@ -6,32 +6,56 @@ using System.Linq;
 AppDataInput inputFile = new AppDataInput();
 List<string> dataList = inputFile.ReadData("Day03-input.txt"); //Read data from: "../../AdventOfCode2020/Data/{dataInputFile}"
 
-int numberOfTrees = 0;
-int x = 0;
+long multiplyAllTrees = 1;
 
-for (int y = 0; y < dataList.Count; y++)
+//Record for StepsRightDown is created in bottom of this file
+List<StepsRightDown> RightDownList = new List<StepsRightDown>();
+RightDownList.Add(new StepsRightDown(1, 1));
+RightDownList.Add(new StepsRightDown(3, 1));
+RightDownList.Add(new StepsRightDown(5, 1));
+RightDownList.Add(new StepsRightDown(7, 1));
+RightDownList.Add(new StepsRightDown(1, 2));
+
+
+foreach (var map in RightDownList)
 {
-    char[] rowCharArr = dataList[y].ToCharArray();
-    int charPos = x % dataList[y].Count();
+    int numberOfTrees = 0;
+    int x = 0;
 
-    //Format on List<string> dataList == dataList[rowNumber][charPosition] 
-    //This will result in examples: 
-    //first row: dataList[0][0] , second row: dataList[1][3] , third row: dataList[2][6] , ... 
-    //on the twelfth row modulus will start to do some work: 
-    //twelfth row: dataList[11][2] , thirteenth row: dataList[12][5] ...
-    if (dataList[y][charPos] == '#')
-    {
-        rowCharArr[charPos] = 'X'; //Will mark tree as X, in output
-        numberOfTrees++;
-    }
-    else
-    {
-        rowCharArr[charPos] = 'O'; //Will open path as O, in output
-    }
-    x += 3;
+    Console.WriteLine("\n\nWalking on the map: right=" + map.Right + " down=" + map.Down + "\n");
 
-    string newOutputString = new string(rowCharArr);
-    Console.WriteLine(newOutputString);
+    for (int y = 0; y < dataList.Count; y++)
+    {
+        if (y % map.Down is 0)
+        {
+            char[] rowCharArr = dataList[y].ToCharArray();
+            int charPos = x % rowCharArr.Length; //rowCharArr.Length , can also be: dataList[y].Count();
+
+            if (dataList[y][charPos] is '#') // Checks if dataList[row][charPos] is #
+            {
+                rowCharArr[charPos] = 'X'; //Will mark tree as X, in output
+                numberOfTrees++;
+            }
+            else
+            {
+                rowCharArr[charPos] = 'O'; //Will open path as O, in output
+            }
+            x += map.Right;
+
+            string newOutputString = new string(rowCharArr);
+            Console.WriteLine(newOutputString);
+        } else
+        {
+            Console.WriteLine(dataList[y]);
+        }
+    }
+
+    Console.WriteLine("\nNumber of trees: " + numberOfTrees);
+    multiplyAllTrees *= numberOfTrees; //OR same as: multiplyAllTrees = multiplyAllTrees * numberOfTrees;
+    Console.WriteLine("\nAll number before * Number of trees = " + multiplyAllTrees);
 }
 
-Console.WriteLine("\nNumber of trees: " + numberOfTrees);
+Console.WriteLine("\nAnswer: " + multiplyAllTrees);
+
+
+public record StepsRightDown(int Right, int Down); //Creates the Object record: RightDown
