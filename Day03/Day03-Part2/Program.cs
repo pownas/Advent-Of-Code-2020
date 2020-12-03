5482a4145f1b29d0b890ea8760a2f2a78d3d16df
pownas <pownas@outlook.com>
2020-12-03 09:31:19 +0100

    Revert "Found that I can skip System.Linq"
    This reverts commit 039d6c8a454afcfa5533aa07041bb6ab76886928.

--- Day03/Day03-Part2/Program.cs
+++ Day03/Day03-Part2/Program.cs
@@ -1,6 +1,7 @@
 ﻿using AdventOfCode2020;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 AppDataInput inputFile = new AppDataInput();
 List<string> dataList = inputFile.ReadData("Day03-input.txt"); //Read data from: "../../AdventOfCode2020/Data/{dataInputFile}"
