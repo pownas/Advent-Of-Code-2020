ead9364c873626717742430891f430178cc4da35
pownas <pownas@outlook.com>
2020-12-03 06:52:31 +0100

    Added Day03

---
+++ Day03/Day03-Part1/Program.cs
@@ -0,0 +1,11 @@
+﻿using AdventOfCode2020;
+using System;
+using System.Collections.Generic;
+
+AppDataInput inputFile = new AppDataInput();
+List<string> dataList = inputFile.ReadData("Day02-input.txt"); //Read data from: "../../AdventOfCode2020/Data/{dataInputFile}"
+
+
+
+Console.WriteLine("Hello World!");
+
