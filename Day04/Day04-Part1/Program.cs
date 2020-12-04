a48a564a1012bf4c56d7d0cc0cc1ea36e60857d7
pownas <pownas@outlook.com>
2020-12-04 07:23:21 +0100

    Started adding some example data

--- Day04/Day04-Part1/Program.cs
+++ Day04/Day04-Part1/Program.cs
@@ -6,7 +6,21 @@ using System.Linq;
 AppDataInput inputFile = new AppDataInput();
 List<string> dataList = inputFile.ReadData("Day03-input.txt"); //Read data from: "../../AdventOfCode2020/Data/{dataInputFile}"
 
-Passport pass = new Passport();
+Passport pass = new Passport(860033327, 147, 1937, 2017, 2020, 183);
+
+ecl: gry pid: eyr:  hcl:#fffffd
+byr:  iyr:  cid: 147 hgt: cm
+
+    iyr:2013 ecl: amb cid:350 eyr: 2023 pid: 028048884
+hcl:#cfa07d byr:1929
+
+hcl:#ae17e1 iyr:2013
+eyr: 2024
+ecl: brn pid:760753108 byr: 1931
+hgt: 179cm
+
+ hcl:#cfa07d eyr:2025 pid:166559648
+iyr: 2011 ecl: brn hgt:59in
 
 
 /// <summary>
