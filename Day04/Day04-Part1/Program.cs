fa85e9f0e947879a011f976b47bf28604661e89d
pownas <pownas@outlook.com>
2020-12-04 07:22:34 +0100

    Changed from class to record

--- Day04/Day04-Part1/Program.cs
+++ Day04/Day04-Part1/Program.cs
@@ -7,3 +7,16 @@ AppDataInput inputFile = new AppDataInput();
 List<string> dataList = inputFile.ReadData("Day03-input.txt"); //Read data from: "../../AdventOfCode2020/Data/{dataInputFile}"
 
 Passport pass = new Passport();
+
+
+/// <summary>
+///     pid(Passport ID), 
+///     cid(Country ID), 
+///     byr(Birth Year), 
+///     iyr(Issue Year), 
+///     eyr(Expiration Year), 
+///     hgt(Height), 
+///     hcl(Hair Color), 
+///     ecl(Eye Color)
+/// </summary>
+public record Passport(int PassID, int CountryID, int BirthYear, int IssuedYear, int ExpYear, int Height, string HairColor, string EyeColor);
