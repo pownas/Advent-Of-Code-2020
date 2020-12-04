f07ee53e3c62a1fc254a30614fb019922141f942
pownas <pownas@outlook.com>
2020-12-04 07:21:46 +0100

    Adding Day4 part1

---
+++ Day04/Day04-Part1/Program.cs
@@ -0,0 +1,9 @@
+﻿using AdventOfCode2020;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+AppDataInput inputFile = new AppDataInput();
+List<string> dataList = inputFile.ReadData("Day03-input.txt"); //Read data from: "../../AdventOfCode2020/Data/{dataInputFile}"
+
+Passport pass = new Passport();
