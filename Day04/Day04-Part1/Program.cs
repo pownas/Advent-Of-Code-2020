95fe3064dd891749d1c18d8c672ad308679dbde6
pownas <pownas@outlook.com>
2020-12-04 12:50:26 +0100

    Adding working code

--- Day04/Day04-Part1/Program.cs
+++ Day04/Day04-Part1/Program.cs
@@ -4,146 +4,175 @@ using System.Collections.Generic;
 using System.Linq;
 
 AppDataInput inputFile = new AppDataInput();
-List<string> dataList = inputFile.ReadData("Day04-input.txt"); //Read data from: "../../AdventOfCode2020/Data/{dataInputFile}"
-List <Passport> passportList = new List<Passport>();
-int totalNumberOfPassports = 0;
+List<Dictionary<string, string>> passportList = inputFile.ReadKeyValue(@"Day04-input.txt", " ", ":");
+
 int validNumberOfPassports = 0;
-
-////Examples:
-//Passport pass1 = new Passport(860033327, 147, 1937, 2017, 2020, 183, "CM", "#fffffd", "gry");
-//Passport pass2 = new Passport(028048884, 350, 1929, 2013, 2023, 0, "NONE", "#cfa07d", "amb");
-//Passport pass3 = new Passport(760753108, 147, 1931, 2013, 2024, 179, "CM", "#ae17e1", "brn");
-//Passport pass4 = new Passport(166559648, 0, 1937, 2011, 2025, 59, "IN", "#cfa07d", "brn");
-
-//passportList.Add(pass1);
-//passportList.Add(pass2);
-//passportList.Add(pass3);
-//passportList.Add(pass4);
-
-ReadPassportBatch();
-
 
 foreach (var passport in passportList)
 {
-    Console.WriteLine("pid:" + passport.pid + ", cid:" + passport.cid + ", byr:" + passport.byr + ", iyr:" + passport.iyr + ", eyr:" + passport.eyr + ", hgt:" + passport.hgt + ", hgtINCM:" + passport.hgtINCM + ", hcl:" + passport.hcl + ", ecl:" + passport.ecl);
-    validNumberOfPassports++;
-}
-
-Console.WriteLine("\nTotal number of passports: " + totalNumberOfPassports);
-Console.WriteLine("\nANSWER - Valid passports: " + validNumberOfPassports);
-
-
-
-
-void ReadPassportBatch()
-{
-    for (int i = 0; i < dataList.Count; i++)
+    int n = 0;
+    n += passport.ContainsKey("pid") ? 1 : 0;
+    n += passport.ContainsKey("byr") ? 1 : 0;
+    n += passport.ContainsKey("iyr") ? 1 : 0;
+    n += passport.ContainsKey("eyr") ? 1 : 0;
+    n += passport.ContainsKey("hgt") ? 1 : 0;
+    n += passport.ContainsKey("hcl") ? 1 : 0;
+    n += passport.ContainsKey("ecl") ? 1 : 0;
+    if (n >= 7)
     {
-        if (dataList[i] is not "" || dataList[i] is not null)
-        {
-            totalNumberOfPassports++;
-
-            string[] variables = new string[] { "pid", "cid", "byr", "iyr", "eyr", "hgt", "hgtINCM", "hcl", "ecl" };
-            int pid = 0;
-            int cid = 0;
-            int byr = 0;
-            int iyr = 0;
-            int eyr = 0;
-            int hgt = 0;
-            string hgtINCM = "NONE";
-            string hcl = "NONE";
-            string ecl = "NONE";
-            for (int passRow = i; passRow < (i+5); passRow++)
-            {
-                if (passRow == (dataList.Count - 1))
-                {
-                    passRow += 15;
-                    break;
-                }
-
-                if (dataList[passRow] is not "" && dataList[passRow] is not null)
-                {
-                    foreach (var variable in variables)
-                    {
-                        if (dataList[passRow].Contains(variable))
-                        {
-                            string[] splitted = dataList[passRow].Split(" ");
-                            foreach (var item in splitted)
-                            {
-                                if (item.StartsWith(variable))
-                                {
-                                    string[] splitting = item.Split(":");
-
-                                    if (variable.Equals("pid"))
-                                    {
-                                        int number;
-                                        bool success = Int32.TryParse(splitting[1], out number);
-                                        if (success)
-                                        {
-                                            pid = number;
-                                        }
-                                    }
-                                    else if (variable.Equals("cid"))
-                                        cid = int.Parse(splitting[1]);
-                                    else if (variable.Equals("byr"))
-                                        byr = int.Parse(splitting[1]);
-                                    else if (variable.Equals("iyr"))
-                                        iyr = int.Parse(splitting[1]);
-                                    else if (variable.Equals("eyr"))
-                                        eyr = int.Parse(splitting[1]);
-                                    else if (variable.Equals("hgt"))
-                                    {
-                                        //have to splitt value
-                                        string[] hgtSplitt = splitting[1].Split("i");
-                                        hgtSplitt = hgtSplitt[0].Split("c");
-
-                                        hgt = int.Parse(hgtSplitt[0]);
-
-                                        if (hgtINCM is "NONE")
-                                        {
-                                            if (splitting[1].Contains("in"))
-                                                hgtINCM = "IN";
-                                            else if (splitting[1].Contains("cm"))
-                                                hgtINCM = "CM";
-                                            else
-                                                hgtINCM = "BLANK";
-                                        }
-                                    }
-                                    else if (variable.Equals("hcl"))
-                                        hcl = splitting[1];
-                                    else if (variable.Equals("ecl"))
-                                        ecl = splitting[1];
-                                }
-                            }
-                        }
-                    }
-                }
-                //  
-                //optional: && cid is not 0
-                if (pid is not 0 && byr is not 0 && iyr is not 0 && eyr is not 0 && hgt is not 0 && hgtINCM is not "NONE" && hcl is not "NONE" && ecl is not "NONE")
-                {
-                    Passport passport = new Passport(pid, cid, byr, iyr, eyr, hgt, hgtINCM, hcl, ecl);
-
-                    if (passportList.Exists(x => x.pid == passport.pid) is false)
-                    {
-                        passportList.Add(passport);
-                        i = passRow; //jumps to the row where I'm at now
-                    }
-                    passRow += 10; //add some rows to inner loop to quit this loop.
-                }
-            }
-        }
+        validNumberOfPassports++;
+        Console.Write("\n valid - ");
     }
-}
+    else
+        Console.Write("\n invalid - ");
 
-/// <summary>
-///     pid(Passport ID), 
-///     cid(Country ID), 
-///     byr(Birth Year), 
-///     iyr(Issue Year), 
-///     eyr(Expiration Year), 
-///     hgt(Height), 
-///     hcl(Hair Color), 
-///     ecl(Eye Color)
-/// </summary>
-public record Passport(int pid, int cid, int byr, int iyr, int eyr, int hgt, string hgtINCM, string hcl, string ecl);
+    foreach (var keyValue in passport)
+        Console.Write(keyValue.Key + ":" + keyValue.Value + "    ");
+}
+Console.WriteLine("\n\nTotal number of passports: " + passportList.Count);
+
+Console.WriteLine("\nANSWER - Number of valid passwords: " + validNumberOfPassports);
+
+
+//List<string> dataList = inputFile.ReadData("Day04-input.txt"); //Read data from: "../../AdventOfCode2020/Data/{dataInputFile}"
+//List <Passport> passportList = new List<Passport>();
+//int totalNumberOfPassports = 0;
+//int validNumberOfPassports = 0;
+
+//////Examples:
+////Passport pass1 = new Passport(860033327, 147, 1937, 2017, 2020, 183, "CM", "#fffffd", "gry");
+////Passport pass2 = new Passport(028048884, 350, 1929, 2013, 2023, 0, "NONE", "#cfa07d", "amb");
+////Passport pass3 = new Passport(760753108, 147, 1931, 2013, 2024, 179, "CM", "#ae17e1", "brn");
+////Passport pass4 = new Passport(166559648, 0, 1937, 2011, 2025, 59, "IN", "#cfa07d", "brn");
+
+////passportList.Add(pass1);
+////passportList.Add(pass2);
+////passportList.Add(pass3);
+////passportList.Add(pass4);
+
+//ReadPassportBatch();
+
+
+//foreach (var passport in passportList)
+//{
+//    Console.WriteLine("pid:" + passport.pid + ", cid:" + passport.cid + ", byr:" + passport.byr + ", iyr:" + passport.iyr + ", eyr:" + passport.eyr + ", hgt:" + passport.hgt + ", hgtINCM:" + passport.hgtINCM + ", hcl:" + passport.hcl + ", ecl:" + passport.ecl);
+//    validNumberOfPassports++;
+//}
+
+//Console.WriteLine("\nTotal number of passports: " + totalNumberOfPassports);
+//Console.WriteLine("\nANSWER - Valid passports: " + validNumberOfPassports);
+
+
+
+//void ReadPassportBatch()
+//{
+//    for (int i = 0; i < dataList.Count; i++)
+//    {
+//        if (dataList[i] is not "" || dataList[i] is not null)
+//        {
+//            totalNumberOfPassports++;
+
+//            string[] variables = new string[] { "pid", "cid", "byr", "iyr", "eyr", "hgt", "hgtINCM", "hcl", "ecl" };
+//            int pid = 0;
+//            int cid = 0;
+//            int byr = 0;
+//            int iyr = 0;
+//            int eyr = 0;
+//            int hgt = 0;
+//            string hgtINCM = "NONE";
+//            string hcl = "NONE";
+//            string ecl = "NONE";
+//            for (int passRow = i; passRow < (i+5); passRow++)
+//            {
+//                if (passRow == (dataList.Count - 1))
+//                {
+//                    passRow += 15;
+//                    break;
+//                }
+
+//                if (dataList[passRow] is not "" && dataList[passRow] is not null)
+//                {
+//                    foreach (var variable in variables)
+//                    {
+//                        if (dataList[passRow].Contains(variable))
+//                        {
+//                            string[] splitted = dataList[passRow].Split(" ");
+//                            foreach (var item in splitted)
+//                            {
+//                                if (item.StartsWith(variable))
+//                                {
+//                                    string[] splitting = item.Split(":");
+
+//                                    if (variable.Equals("pid"))
+//                                    {
+//                                        int number;
+//                                        bool success = Int32.TryParse(splitting[1], out number);
+//                                        if (success)
+//                                        {
+//                                            pid = number;
+//                                        }
+//                                    }
+//                                    else if (variable.Equals("cid"))
+//                                        cid = int.Parse(splitting[1]);
+//                                    else if (variable.Equals("byr"))
+//                                        byr = int.Parse(splitting[1]);
+//                                    else if (variable.Equals("iyr"))
+//                                        iyr = int.Parse(splitting[1]);
+//                                    else if (variable.Equals("eyr"))
+//                                        eyr = int.Parse(splitting[1]);
+//                                    else if (variable.Equals("hgt"))
+//                                    {
+//                                        //have to splitt value
+//                                        string[] hgtSplitt = splitting[1].Split("i");
+//                                        hgtSplitt = hgtSplitt[0].Split("c");
+
+//                                        hgt = int.Parse(hgtSplitt[0]);
+
+//                                        if (hgtINCM is "NONE")
+//                                        {
+//                                            if (splitting[1].Contains("in"))
+//                                                hgtINCM = "IN";
+//                                            else if (splitting[1].Contains("cm"))
+//                                                hgtINCM = "CM";
+//                                            else
+//                                                hgtINCM = "BLANK";
+//                                        }
+//                                    }
+//                                    else if (variable.Equals("hcl"))
+//                                        hcl = splitting[1];
+//                                    else if (variable.Equals("ecl"))
+//                                        ecl = splitting[1];
+//                                }
+//                            }
+//                        }
+//                    }
+//                }
+//                //  
+//                //optional: && cid is not 0
+//                if (pid is not 0 && byr is not 0 && iyr is not 0 && eyr is not 0 && hgt is not 0 && hgtINCM is not "NONE" && hcl is not "NONE" && ecl is not "NONE")
+//                {
+//                    Passport passport = new Passport(pid, cid, byr, iyr, eyr, hgt, hgtINCM, hcl, ecl);
+
+//                    if (passportList.Exists(x => x.pid == passport.pid) is false)
+//                    {
+//                        passportList.Add(passport);
+//                        i = passRow; //jumps to the row where I'm at now
+//                    }
+//                    passRow += 10; //add some rows to inner loop to quit this loop.
+//                }
+//            }
+//        }
+//    }
+//}
+
+///// <summary>
+/////     pid(Passport ID), 
+/////     cid(Country ID), 
+/////     byr(Birth Year), 
+/////     iyr(Issue Year), 
+/////     eyr(Expiration Year), 
+/////     hgt(Height), 
+/////     hcl(Hair Color), 
+/////     ecl(Eye Color)
+///// </summary>
+//public record Passport(int pid, int cid, int byr, int iyr, int eyr, int hgt, string hgtINCM, string hcl, string ecl);
