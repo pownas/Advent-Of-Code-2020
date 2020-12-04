f409e8ef713ccc8b0098e105fb413bc0c3c6cbf4
pownas <pownas@outlook.com>
2020-12-04 12:49:04 +0100

    Adding appdatareader

--- AdventOfCode2020-InputDataReader/AppDataInput.cs
+++ AdventOfCode2020-InputDataReader/AppDataInput.cs
@@ -22,5 +22,33 @@ namespace AdventOfCode2020
             }
             return returnString;
         }
+
+        public List<Dictionary<string, string>> ReadKeyValue(string dataInputFile, string separator1, string separator2)
+        {
+            StreamReader reader = File.OpenText(folderDataPath + dataInputFile);
+            Dictionary<string, string> dictionary = new Dictionary<string, string>();
+            List<Dictionary<string, string>> dictionaryList = new List<Dictionary<string, string>>();
+            string row;
+            while ((row = reader.ReadLine()) is not null)
+            {
+                if (row is "")
+                {
+                    dictionaryList.Add(dictionary);
+                    dictionary = new Dictionary<string, string>();
+                }
+                else
+                {
+                    string[] splitted = row.Split(separator1).ToArray();
+                    foreach (string s in splitted)
+                    {
+                        string[] ss = s.Split(separator2).ToArray();
+                        dictionary[ss[0]] = ss[1];
+                    }
+                }
+            }
+            if (dictionary.Count() > 0)
+                dictionaryList.Add(dictionary);
+            return dictionaryList;
+        }
     }
 }
